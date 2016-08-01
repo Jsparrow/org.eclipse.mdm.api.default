97183eb3beeda23998658f0f7f23656d96befa66
Viktor Stöhr <viktor.stoehr@gigatronik.com>
2016-08-01 06:57:42 +0200

    javadocs for Project & Pool added

--- src/main/java/org/eclipse/mdm/api/dflt/model/Pool.java
+++ src/main/java/org/eclipse/mdm/api/dflt/model/Pool.java
@@ -5,11 +5,40 @@ import org.eclipse.mdm.api.base.model.Core;
 import org.eclipse.mdm.api.base.model.Deletable;
 import org.eclipse.mdm.api.base.model.Test;
 
+/**
+ * Implementation of the pool entity type. The pool entity is used to establish
+ * a further navigation structure through the stored measurement data. Its name
+ * may be freely chosen but has to to be unique. A project contains {@link Test}
+ * entities as its children.
+ *
+ * @since 1.0.0
+ * @author Viktor Stoehr, Gigatronik Ingolstadt GmbH
+ */
 public final class Pool extends BaseEntity implements Deletable {
 
+	// ======================================================================
+	// Class variables
+	// ======================================================================
+
+	/**
+	 * The {@link Project} parent type.
+	 */
 	public static final Class<Project> PARENT_TYPE_PROJECT = Project.class;
+
+	/**
+	 * The {@link Test} child type.
+	 */
 	public static final Class<Test> CHILD_TYPE_TEST = Test.class;
 
+	// ======================================================================
+	// Constructors
+	// ======================================================================
+
+	/**
+	 * Constructor.
+	 *
+	 * @param core The {@link Core}.
+	 */
 	Pool(Core core) {
 		super(core);
 	}
--- src/main/java/org/eclipse/mdm/api/dflt/model/Project.java
+++ src/main/java/org/eclipse/mdm/api/dflt/model/Project.java
@@ -4,10 +4,34 @@ import org.eclipse.mdm.api.base.model.BaseEntity;
 import org.eclipse.mdm.api.base.model.Core;
 import org.eclipse.mdm.api.base.model.Deletable;
 
+/**
+ * Implementation of the project entity type. The project is the top level of
+ * stored measurement data. Its name may be freely chosen but has to to be
+ * unique. A project contains {@link Pool} entities as its children.
+ *
+ * @since 1.0.0
+ * @author Viktor Stoehr, Gigatronik Ingolstadt GmbH
+ */
 public final class Project extends BaseEntity implements Deletable {
 
+	// ======================================================================
+	// Class variables
+	// ======================================================================
+
+	/**
+	 * The {@link Pool} child type.
+	 */
 	public static final Class<Pool> CHILD_TYPE_POOL = Pool.class;
 
+	// ======================================================================
+	// Constructors
+	// ======================================================================
+
+	/**
+	 * Constructor.
+	 *
+	 * @param core The {@link Core}.
+	 */
 	Project(Core core) {
 		super(core);
 	}

d30e1fc4196efba3daa45d7fe95a13b81031709e
Viktor Stöhr <viktor.stoehr@gigatronik.com>
2016-08-02 07:18:01 +0200

    javadocs to Status added

--- src/main/java/org/eclipse/mdm/api/dflt/model/Status.java
+++ src/main/java/org/eclipse/mdm/api/dflt/model/Status.java
@@ -15,25 +15,60 @@ import org.eclipse.mdm.api.base.model.BaseEntity;
 import org.eclipse.mdm.api.base.model.Core;
 import org.eclipse.mdm.api.base.model.Describable;
 import org.eclipse.mdm.api.base.model.StatusAttachable;
+import org.eclipse.mdm.api.base.model.Test;
+import org.eclipse.mdm.api.base.model.TestStep;
 
+/**
+ * Implementation of the status entity type. A status may be attached to {@link
+ * StatusAttachable} entities like {@link Test} or {@link TestStep} to indicate
+ * they have a state in a complete workflow.
+ *
+ * @since 1.0.0
+ * @author Viktor Stoehr, Gigatronik Ingolstadt GmbH
+ */
 public final class Status extends BaseEntity implements Describable {
 
 	// ======================================================================
 	// Constructors
 	// ======================================================================
 
+	/**
+	 * Constructor.
+	 *
+	 * @param core The {@link Core}.
+	 */
 	Status(Core core) {
 		super(core);
 	}
 
+	// ======================================================================
+	// Public methods
+	// ======================================================================
+
+	/**
+	 * Assigns this status to given {@link StatusAttachable}s.
+	 *
+	 * @param statusAttachables This status will be assigned to all of them.
+	 */
 	public <T extends StatusAttachable> void assign(List<T> statusAttachables) {
 		statusAttachables.forEach(this::assign);
 	}
 
+	/**
+	 * Assigns this status to given {@link StatusAttachable}.
+	 *
+	 * @param statusAttachable This status will be assigned to it.
+	 */
 	public <T extends StatusAttachable> void assign(T statusAttachable) {
 		getCore(statusAttachable).getMutableStore().set(this);
 	}
 
+	/**
+	 * Returns the {@link Status} attached to given {@link StatusAttachable}.
+	 *
+	 * @param statusAttachable The {@code StatusAttachable}.
+	 * @return Optional is empty if no {@code Status} is attached.
+	 */
 	public static Optional<Status> of(StatusAttachable statusAttachable) {
 		return Optional.ofNullable(getCore(statusAttachable).getMutableStore().get(Status.class));
 	}

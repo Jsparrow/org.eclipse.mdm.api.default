b79bf74abe7e5d529ab7468130c22efb82a93b32
Fabian Dreier <fabian.dreier@karakun.com>
2018-10-31 14:32:16 +0100

    Bug 529729: Added new entities needed for status attachment Signed-off-by: Fabian Dreier <fabian.dreier@karakun.com>

---
+++ src/main/java/org/eclipse/mdm/api/dflt/model/Classification.java
@@ -0,0 +1,62 @@
+/********************************************************************************
+ * Copyright (c) 2015-2018 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ ********************************************************************************/
+package org.eclipse.mdm.api.dflt.model;
+
+import org.eclipse.mdm.api.base.adapter.Core;
+import org.eclipse.mdm.api.base.model.BaseEntity;
+import org.eclipse.mdm.api.base.model.StatusAttachable;
+
+import java.util.List;
+
+public class Classification
+        extends BaseEntity  {
+
+    // ======================================================================
+    // Constructors
+    // ======================================================================
+
+    /**
+     * Constructor.
+     *
+     * @param core
+     *            The {@link Core}.
+     */
+    Classification(Core core) {
+        super(core);
+    }
+
+    /**
+     * Assigns this status to given {@link StatusAttachable}s.
+     *
+     * @param <T>
+     *            The status attachable type.
+     * @param statusAttachables
+     *            This status will be assigned to all of them.
+     */
+    public <T extends StatusAttachable> void assign(List<T> statusAttachables) {
+        statusAttachables.forEach(this::assign);
+    }
+
+    /**
+     * Assigns this status to given {@link StatusAttachable}.
+     *
+     * @param <T>
+     *            The status attachable type.
+     * @param statusAttachable
+     *            This status will be assigned to it.
+     */
+    public <T extends StatusAttachable> void assign(T statusAttachable) {
+        getCore(statusAttachable).getMutableStore().set(this);
+    }
+}
